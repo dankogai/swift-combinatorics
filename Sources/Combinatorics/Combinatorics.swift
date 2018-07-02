a6f6d80fa9793a09febae177a489e13e253906ff
Dan Kogai <dankogai+github@gmail.com>
2018-07-02 12:44:43 +0900

    Make subscript index generic

--- Sources/Combinatorics/Combinatorics.swift
+++ Sources/Combinatorics/Combinatorics.swift
@@ -30,149 +30,157 @@ public class Combinatorics {
 }
 public protocol CombinatoricsType {
     associatedtype SubElement
-    init(seed:[SubElement], size:Int)
-    var count:Int { get }
-    subscript<I:SignedInteger>(_:I)->[SubElement]{ get }
+    associatedtype Index:SignedInteger
+    init(seed:[SubElement], size:Index)
+    var count:Index { get }
+    subscript(_:Index)->[SubElement]{ get }
 }
 extension CombinatoricsType {
     public typealias Element = [SubElement]
     public func makeIterator() -> AnyIterator<Element> {
-        var idx = -1
+        var idx:Index = -1
         return AnyIterator {
             idx += 1
             guard idx < self.count else { return nil }
             return self[idx]
         }
     }
-    public init<S:Sequence>(of:S, size:Int=64) where S.Element == SubElement {
+    public init<S:Sequence>(of:S, size:Index=64) where S.Element == SubElement {
         self.init(seed:Array(of), size:size)
     }
     public init(_ source:SubElement...) {
         self.init(seed:source, size:64)
     }
 }
-/// permutation
-public struct Permutation<SubElement> : CombinatoricsType, Sequence {
-    public let seed:[SubElement] // immutable
-    public let size:Int
-    public let count:Int
-    public init(seed:[SubElement], size:Int=64) {
-        guard 0 < size else { fatalError() }
-        self.seed  = seed
-        self.size  = Swift.min(size, seed.count)
-        self.count = Combinatorics.permutation(seed.count, size)
+/// This is to wrap Index
+public struct CombinatoricsIndex<Index:SignedInteger> {
+    /// permutation
+    public struct Permutation<SubElement> : CombinatoricsType, Sequence {
+        public let seed:[SubElement] // immutable
+        public let size:Index
+        public let count:Index
+        public init(seed:[SubElement], size:Index=64) {
+            guard 0 < size else { fatalError() }
+            self.seed  = seed
+            self.size  = Swift.min(size, Index(seed.count))
+            self.count = Combinatorics.permutation(Index(seed.count), size)
+        }
+        public subscript(_ idx:Index)->[SubElement] {
+            guard 0 <= idx && idx < count else { fatalError("Index out of range") }
+            guard 1 < size else {
+                return [seed[Int(idx)]]
+            }
+            let skip   = Combinatorics.factorial(Index(seed.count) - size)
+            let digits = Combinatorics.factoradic(idx * skip, Index(seed.count))
+            var source = seed
+            var result = [SubElement]()
+            for i in 0 ..< Int(size) {
+                result.append(source.remove(at: digits[i]))
+            }
+            return result
+        }
     }
-    public subscript<I:SignedInteger>(_ idx:I)->[SubElement] {
-        guard 0 <= idx && idx < count else { fatalError("Index out of range") }
-        guard 1 < size else {
-            return [seed[Int(idx)]]
+    /// combination
+    public struct Combination<SubElement> : CombinatoricsType, Sequence {
+        public let perm:Permutation<SubElement>
+        public let count:Index
+        public init(seed:[SubElement], size:Index=64) {
+            perm  = Permutation(seed:seed, size:size)
+            count = Combinatorics.combination(perm.size, Index(perm.seed.count))
         }
-        let skip   = Combinatorics.factorial(seed.count - size)
-        let digits = Combinatorics.factoradic(idx * I(skip), I(seed.count))
-        var source = seed
-        var result = [SubElement]()
-        for i in 0 ..< size {
-            result.append(source.remove(at: digits[i]))
+        public subscript(_ idx:Index)->[SubElement] {
+            guard 0 <= idx && idx < count else { fatalError("Index out of range") }
+            // cf. https://en.wikipedia.org/wiki/Combinatorial_number_system
+            func findIndex(_ n:Index)->Index {
+                guard 2 < n else { return n }
+                let p = n - 1
+                let s = p & -p
+                let r = p + s
+                let t = r & -r
+                let m = ((t / s) >> 1) - 1
+                return r | m
+            }
+            return perm[findIndex(idx)]
         }
-        return result
+    }
+    /// BaseN
+    public struct BaseN<SubElement> : CombinatoricsType, Sequence {
+        public let seed:[SubElement] // immutable
+        public let size:Index
+        public let count:Index
+        public init(seed:[SubElement], size:Index=64) {
+            guard 0 < size else { fatalError() }
+            self.seed  = seed
+            self.size  = Swift.min(size, Index(seed.count))
+            self.count =  (0..<Int(size)).reduce(Index(1)){ n,_ in n * Index(seed.count) }
+        }
+        public subscript<I:SignedInteger>(_ idx:I)->[SubElement] {
+            guard 0 <= idx && idx < count else { fatalError("Index out of range") }
+            var result = [SubElement]()
+            var (q, r) = (Int(idx), 0)
+            for _ in 0 ..< Int(size) {
+                (q, r) = q.quotientAndRemainder(dividingBy: seed.count)
+                result.append(seed[r])
+            }
+            return result
+        }
+    }
+    /// Power Set
+    public struct PowerSet<SubElement> : CombinatoricsType, Sequence {
+        public let seed:[SubElement] // immutable
+        public let count:Index
+        public init(seed:[SubElement], size:Index=64) {
+            guard 0 < size else { fatalError() }
+            self.seed  = seed
+            self.count = (0..<seed.count).reduce(Index(1)){ n,_ in n * Index(2) }
+        }
+        public subscript<I:SignedInteger>(_ idx:I)->[SubElement] {
+            guard 0 <= idx && idx < count else { fatalError("Index out of range") }
+            var result = [SubElement]()
+            for i in 0..<seed.count {
+                if idx & (1 << i) != 0 { result.append(seed[i]) }
+            }
+            return result
+        }
+    }
+    /// CartesianProduct
+    /// is slightly but significantly different from the rest so it DOES NOT CONFORM TO CombinatoricsType
+    public struct CartesianProduct<SubElement>: Sequence where SubElement:Sequence  {
+        public typealias Element = [SubElement.Element]
+        public let seed:[[SubElement.Element]] // immutable
+        public var size:Index { return Index(seed.count) }
+        public let count:Index
+        public init(seed:[SubElement]) {
+            guard !seed.isEmpty else { fatalError() }
+            self.seed = seed.map{ Array($0) }
+            self.count = self.seed.reduce(Index(1)){ $0 * Index($1.count) }
+        }
+        public init(_ source:SubElement...) {
+            self.init(seed:source)
+        }
+        public subscript<I:SignedInteger>(_ idx:I)->[SubElement.Element] {
+            guard 0 <= idx && idx < count else { fatalError("Index out of range") }
+            var result = [SubElement.Element]()
+            var (q, r) = (Int(idx), 0)
+            for i in 0..<Int(size) {
+                (q, r) = q.quotientAndRemainder(dividingBy: seed[i].count)
+                let e = seed[i][r]
+                result.append(e)
+            }
+            return result
+        }
+        public func makeIterator() -> AnyIterator<Element> {
+            var idx = -1
+            return AnyIterator {
+                idx += 1
+                guard idx < self.count else { return nil }
+                return self[idx]
+            }
+        }
     }
 }
-/// combination
-public struct Combination<SubElement> : CombinatoricsType, Sequence {
-    public let perm:Permutation<SubElement>
-    public let count:Int
-    public init(seed:[SubElement], size:Int=64) {
-        perm  = Permutation(seed:seed, size:size)
-        count = Combinatorics.combination(perm.size, perm.seed.count)
-    }
-    public subscript<I:SignedInteger>(_ idx:I)->[SubElement] {
-        guard 0 <= idx && idx < count else { fatalError("Index out of range") }
-        // cf. https://en.wikipedia.org/wiki/Combinatorial_number_system
-        func findIndex(_ n:I)->I {
-            guard 2 < n else { return n }
-            let p = n - 1
-            let s = p & -p
-            let r = p + s
-            let t = r & -r
-            let m = ((t / s) >> 1) - 1
-            return r | m
-        }
-        return perm[findIndex(idx)]
-    }
-}
-/// BaseN
-public struct BaseN<SubElement> : CombinatoricsType, Sequence {
-    public let seed:[SubElement] // immutable
-    public let size:Int
-    public let count:Int
-    public init(seed:[SubElement], size:Int=64) {
-        guard 0 < size else { fatalError() }
-        self.seed  = seed
-        self.size  = Swift.min(size, seed.count)
-        self.count =  (0..<size).reduce(1){ n,_ in n * seed.count }
-    }
-    public subscript<I:SignedInteger>(_ idx:I)->[SubElement] {
-        guard 0 <= idx && idx < count else { fatalError("Index out of range") }
-        var result = [SubElement]()
-        var (q, r) = (Int(idx), 0)
-        for _ in 0..<size {
-            (q, r) = q.quotientAndRemainder(dividingBy: seed.count)
-            result.append(seed[r])
-        }
-        return result
-    }
-}
-/// Power Set
-public struct PowerSet<SubElement> : CombinatoricsType, Sequence {
-    public let seed:[SubElement] // immutable
-    public let count:Int
-    public init(seed:[SubElement], size:Int=64) {
-        guard 0 < size else { fatalError() }
-        self.seed  = seed
-        self.count = (0..<seed.count).reduce(1){ n,_ in n * 2 }
-    }
-    public subscript<I:SignedInteger>(_ idx:I)->[SubElement] {
-        guard 0 <= idx && idx < count else { fatalError("Index out of range") }
-        var result = [SubElement]()
-        for i in 0..<seed.count {
-            if idx & (1 << i) != 0 { result.append(seed[i]) }
-        }
-        return result
-    }
-}
-/// CartesianProduct
-/// is slightly but significantly different from the rest so it DOES NOT CONFORM TO CombinatoricsType
-public struct CartesianProduct<SubElement>: Sequence where SubElement:Sequence  {
-    public typealias Element = [SubElement.Element]
-    public let seed:[[SubElement.Element]] // immutable
-    public var size:Int { return seed.count }
-    public let count:Int
-    public init(seed:[SubElement]) {
-        guard !seed.isEmpty else { fatalError() }
-        self.seed = seed.map{ Array($0) }
-        self.count = self.seed.reduce(1){ $0 * $1.count }
-    }
-    public init(_ source:SubElement...) {
-        self.init(seed:source)
-    }
-    public subscript<I:SignedInteger>(_ idx:I)->[SubElement.Element] {
-        guard 0 <= idx && idx < count else { fatalError("Index out of range") }
-        var result = [SubElement.Element]()
-        var (q, r) = (Int(idx), 0)
-        for i in 0..<size {
-            (q, r) = q.quotientAndRemainder(dividingBy: seed[i].count)
-            let e = seed[i][r]
-            result.append(e)
-        }
-        return result
-    }
-    public func makeIterator() -> AnyIterator<Element> {
-        var idx = -1
-        return AnyIterator {
-            idx += 1
-            guard idx < self.count else { return nil }
-            return self[idx]
-        }
-    }
-}
-
+public typealias Permutation        = CombinatoricsIndex<Int>.Permutation
+public typealias Combination        = CombinatoricsIndex<Int>.Combination
+public typealias BaseN              = CombinatoricsIndex<Int>.BaseN
+public typealias PowerSet           = CombinatoricsIndex<Int>.PowerSet
+public typealias CartesianProduct   = CombinatoricsIndex<Int>.CartesianProduct
