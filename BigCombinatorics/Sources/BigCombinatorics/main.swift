959fb7cc9688b47b11b2a609c3dc176b6ec4e91c
Dan Kogai <dankogai+github@gmail.com>
2020-08-10 19:25:02 +0900

    refactor main.swift

--- BigCombinatorics/Sources/BigCombinatorics/main.swift
+++ BigCombinatorics/Sources/BigCombinatorics/main.swift
@@ -5,8 +5,14 @@ typealias BigPermutation = CombinatoricsIndex<BigInt>.Permutation
 typealias BigCombination = CombinatoricsIndex<BigInt>.Combination
 
 let p = BigPermutation(of:(0..<100))
-print("p.count ==", p.count)
-print("p.count[p.count - 1] ==", p[p.count - 1])
+print("""
+let p = BigPermutation(of:(0..<100))
+p.count == \(p.count)
+p.count[p.count - 1] == \(p[p.count - 1])
+""")
 let c = BigCombination(of:(0..<100), size:50)
-print("c.count ==", c.count)
-print("c.count[c.count - 1] ==", c[c.count - 1])
+print("""
+let c = BigCombination(of:(0..<100))
+c.count == \(c.count)
+c.count[p.count - 1] == \(c[c.count - 1])
+""")
