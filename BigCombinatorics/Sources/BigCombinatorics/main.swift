f6915a42f9822e6e7d1b8fa2cca958d1876dc412
Dan Kogai <dankogai+github@gmail.com>
2020-08-10 19:04:17 +0900

    add BigCombination

--- BigCombinatorics/Sources/BigCombinatorics/main.swift
+++ BigCombinatorics/Sources/BigCombinatorics/main.swift
@@ -2,7 +2,11 @@ import BigInt
 import Combinatorics
 
 typealias BigPermutation = CombinatoricsIndex<BigInt>.Permutation
+typealias BigCombination = CombinatoricsIndex<BigInt>.Combination
 
 let p = BigPermutation(of:(0..<100))
 print("p.count ==", p.count)
 print("p.count[p.count - 1] ==", p[p.count - 1])
+let c = BigCombination(of:(0..<100), size:50)
+print("c.count ==", c.count)
+print("c.count[c.count - 1] ==", c[c.count - 1])
