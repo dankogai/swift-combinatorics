fb21fbb8cdcc399884bdb0360815dc46b7b6449c
Dan Kogai <dankogai+github@gmail.com>
2026-07-11 19:26:05 +0900

    switch from master to main

--- BigCombinatorics/Package.swift
+++ BigCombinatorics/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "BigCombinatorics",
     dependencies: [
       .package(url:"https://github.com/attaswift/BigInt.git", from:"5.0.0"),
-      .package(url:"..", .branch("master")),
+      .package(url:"..", .branch("main")),
     ],
     targets: [
         .target(
